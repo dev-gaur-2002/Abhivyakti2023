0e43dbcd13365729619313428a148ea60c5238aa
Atharva Rane <atharvarane051102@gmail.com>
2023-02-16 09:16:01 +0530

    add disk section

---
+++ components/DiskBanner/DiskBanner.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styles from "../../styles/DiskBanner.module.css";
+import Image from "next/image";
+
+import { useEffect } from "react";
+
+
+const Landing = () => {
+
+    useEffect(() => {
+      const script = document.createElement("script");
+
+      script.src =
+        "https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js";
+      script.async = true;
+
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }, []);
+
+    useEffect(() => {
+      if (typeof window !== "undefined") {
+        window.$(".popup-youtube").magnificPopup({
+          disableOn: 700,
+          type: "iframe",
+          mainClass: "mfp-fade",
+          removalDelay: 160,
+          preloader: false,
+          fixedContentPos: false,
+        });
+      }
+    });
+
+  return (
+    <div className={styles["page"]}>
+      <div className={styles["section-wrapper"]}>
+        <div className={styles["section-header"]}>
+          <div className={styles["header--line"]} />
+          <div className={styles["header--pill"]}>Abhivyakti</div>
+        </div>
+
+        <div className={styles["text-container"]}>
+          <div className={styles["border-circle"]}>
+            <svg viewBox="58 -42 500 600">
+              <defs>
+                <path id="curve" d="M110 180 A120 115 500 0 1 500 250" />
+              </defs>
+              <text className={styles["section-content"]}>
+                <textPath xlinkHref="#curve">
+                  THE AUGHT-THE ERA OF 2000S
+                </textPath>
+              </text>
+            </svg>
+            <div className={styles["inner-circle"]}></div>
+            {/* <img src="images/play-button.png" className={styles["play-buttom"]}/> */}
+            <div className={styles["play-button"]}>
+              <a
+                class="popup-youtube"
+                href="https://www.youtube.com/watch?v=KMh-sfwSQdE"
+              >
+                <img src="images/play-button.png" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles["section-footer"]}>
+          <div className={styles["footer--line"]} />
+          <div className={styles["footer--text"]}>Abhivyakti 2023</div>
+        </div>
+      </div>
+    </div>
+
+    //   <iframe width="1366" height="485" src="https://www.youtube.com/embed/KMh-sfwSQdE" title="Abhivyakti&#39;23 Theme Reveal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  );
+};
+
+export default Landing;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,31 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Music from '../components/Musicevents/MusicEvents'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Music from "../components/Musicevents/MusicEvents";
+import DiskBanner from "../components/DiskBanner/DiskBanner";
 
-
-import Hero from '../components/Hero/Hero'
-import Landing from './Landing'
-
+import Hero from "../components/Hero/Hero";
+import Landing from "./Landing";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Abhivyakti 2023</title>
+        <script
+          type="text/javascript"
+          src="https://code.jquery.com/jquery-1.12.1.min.js"
+        ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"></script>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css"
+        />
+      </Head>
 
-
-      <Hero/>
-      <Landing/>
-      <Music/> 
-    
-
+      <Hero />
+      <Landing />
+      <DiskBanner />
+      <Music />
     </>
-  )
+  );
 }
