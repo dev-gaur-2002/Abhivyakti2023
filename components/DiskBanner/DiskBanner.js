0a80eacd3730e879df1193318b86102d5d7df664
Atharva Rane <atharvarane051102@gmail.com>
2023-02-18 23:23:09 +0530

    make disk section responsive

--- components/DiskBanner/DiskBanner.js
+++ components/DiskBanner/DiskBanner.js
@@ -2,25 +2,21 @@ import React from "react";
 import styles from "../../styles/DiskBanner.module.css";
 import Image from "next/image";
 
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-
-const Landing = () => {
-
-
+const DiskBanner = () => {
   const [headerFixed, setHeaderFixed] = useState(false);
+  const [videoPlaying, setVideoPlaying] = useState(false);
+
 
   useEffect(() => {
-    
     window.addEventListener("load", () => {
       setHeaderFixed(true);
     });
 
-   
     window.addEventListener("scroll", () => {
       const distanceFromTop = window.scrollY;
 
-      
       if (distanceFromTop >= 770) {
         setHeaderFixed(true);
       } else {
@@ -29,74 +25,44 @@ const Landing = () => {
     });
   }, []);
 
-
-
-    useEffect(() => {
-      const script = document.createElement("script");
-
-      script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js";
-      script.async = true;
-
-      document.body.appendChild(script);
-
-      return () => {
-        document.body.removeChild(script);
-      };
-    }, []);
-
-    useEffect(() => {
-      if (typeof window !== "undefined") {
-        window.$(".popup-youtube").magnificPopup({
-          disableOn: 700,
-          type: "iframe",
-          mainClass: "mfp-fade",
-          removalDelay: 160,
-          preloader: false,
-          fixedContentPos: false,
-        });
-      }
-    });
-
   return (
-    <div className={styles["page"]}>
-      <div className={styles["section-wrapper"]}>
-        <div className={`${styles["section-header"]} ${
+    <div className={styles["wrapper"]}>
+      <div
+        className={`${styles["section-header"]} ${
           headerFixed ? styles["fixed-header"] : ""
-        }`}>
-          <div className={styles["header--line"]} />
-          <div className={styles["header--pill"]}><img src="\images\abhivyakti.svg" className={styles.logo}></img></div>
+        }`}
+      >
+        <div className={styles["header--line"]} />
+        <div className={styles["header--pill"]}>
+          <img src="\images\abhivyakti.svg" className={styles.logo}></img>
         </div>
+      </div>
 
-        <div className={styles["text-container"]}>
-          <div className={styles["border-circle"]}>
-            <svg viewBox="58 -42 500 600">
-              <defs>
-                <path id="curve" d="M110 180 A120 115 500 0 1 500 250" />
-              </defs>
-              <text className={styles["section-content"]}>
-                <textPath xlinkHref="#curve">
-                  THE AUGHT-THE ERA OF 2000S
-                </textPath>
-              </text>
-            </svg>
-            <div className={styles["inner-circle"]}></div>
-            {/* <img src="images/play-button.png" className={styles["play-buttom"]}/> */}
-            <div className={styles["play-button"]}>
-              <a
-                class="popup-youtube"
-                href="https://www.youtube.com/watch?v=KMh-sfwSQdE"
-              >
-                <img src="images/play-button.png" />
-              </a>
-            </div>
-          </div>
+      {videoPlaying ? (
+        <iframe
+          width="80%"
+          height="66%"
+          src="https://www.youtube.com/embed/KMh-sfwSQdE"
+          title="Abhivyakti&#39;23 Theme Reveal"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      ) : (
+        <div className={styles["half-donout"]}>
+          <div className={styles["big-circle"]} />
+          <div className={styles["small-circle"]} />
+          <img
+            className={styles["play-button"]}
+            src="images/play-button.png"
+            onClick={() => setVideoPlaying(true)}
+          />
         </div>
+      )}
 
-        <div className={styles["section-footer"]}>
-          <div className={styles["footer--line"]} />
-          <div className={styles["footer--text"]}>Abhivyakti 2023</div>
-        </div>
+      <div className={styles["section-footer"]}>
+        <div className={styles["footer--line"]} />
+        <div className={styles["footer--text"]}>Abhivyakti 2023</div>
       </div>
     </div>
 
@@ -104,4 +70,4 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+export default DiskBanner;
--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,6 @@ export default function Home() {
     <>
       <Head>
         <title>Abhivyakti 2023</title>
-        <script
-          type="text/javascript"
-          src="https://code.jquery.com/jquery-1.12.1.min.js"
-        ></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"></script>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css"
-        />
       </Head>
       <Hero/>
       <DiskBanner />
